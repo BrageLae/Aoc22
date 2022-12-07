16bae2f19cc50eb58237b6d00ce3446b009b71c7
BrageLae <nicolay@student.chalmers.se>
2022-12-07 20:21:43 +0100

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -3,19 +3,6 @@ import os
 FILE = 'input.txt'
 path = os.path.dirname(__file__)
 
-def binary_search(arr, target):
-    l = 0
-    r = len(arr) - 1
-    while(l <= r):
-        m = (l + r) // 2
-        if target == arr[m]:
-            return m
-        elif target < arr[m]:
-            r = m - 1
-        else:
-            l = m + 1
-    return -1
-
 def calculate_item_value(item: str):
     if(item.islower()):
         return ord(item) - 96
